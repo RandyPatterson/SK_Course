ec34d78d079128a1e56eb0fa544e30783f26dda8
AKS Bytes <code2live@proton.me>
2025-03-30 10:13:53 -0400

    completed

--- module3/AzureAiInference/SK_Console/Program.cs
+++ module3/AzureAiInference/SK_Console/Program.cs
@@ -2,9 +2,10 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Connectors.AzureAIInference;
 
 #pragma warning disable SKEXP0001 
+#pragma warning disable SKEXP0070
 
 namespace SK_DEV
 {
@@ -22,27 +23,24 @@ namespace SK_DEV
 
             // Create a kernel builder and add Azure OpenAI chat completion service
             var builder = Kernel.CreateBuilder();
-            
-            //Azure OpenAI
-            //builder.AddAzureOpenAIChatCompletion(config["modelid"], config["endpoint"], config["apikey"]);
 
-            //OpenAI
-            builder.AddOpenAIChatCompletion(config["OpenAI:modelid"], config["OpenAI:apikey"]);
+            //AI inference
+            builder.AddAzureAIInferenceChatCompletion(config["inference:modelid"], config["inference:apikey"], new Uri(config["inference:endpoint"]));
 
             // Build the kernel
             Kernel kernel = builder.Build();
 
             // Create chat history
-            var history = new ChatHistory();
+            var history = new ChatHistory(systemMessage: "You are a friendly AI Assistant that answers in a friendly manner");
 
             // Get reference to chat completion service
             var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
 
             // Define settings for OpenAI prompt execution
-            OpenAIPromptExecutionSettings settings = new()
+            AzureAIInferencePromptExecutionSettings settings = new()
+
             {
-                ChatSystemPrompt = "You are a friendly AI Assistant that answers in a friendly manner",
-                Temperature = 0.9,
+                Temperature = 0.9f,
                 MaxTokens = 1500,
             };
 
@@ -74,21 +72,9 @@ namespace SK_DEV
                     // Print response to console
                     Console.Write(responseChunk.Content);
                     fullMessage += responseChunk.Content;
-                    usage = ((OpenAI.Chat.StreamingChatCompletionUpdate)responseChunk.InnerContent).Usage;
                 }
                 // Add response to chat history
                 history.AddAssistantMessage(fullMessage);
-
-
-                //get non-streaming result from chat completion setvice
-                //var response = await chatCompletionService.GetChatMessageContentAsync(history, settings);
-                //add response to chat history
-                //history.Add(response);
-                
-                // Display number of tokens used (model specific)
-                Console.WriteLine($"\n\tInput Tokens: \t{usage?.InputTokenCount}");
-                Console.WriteLine($"\tOutput Tokens: \t{usage?.OutputTokenCount}");
-                Console.WriteLine($"\tTotal Tokens: \t{usage?.TotalTokenCount}");
 
                 // Reduce chat history if necessary
                 var reduceMessages = await reducer.ReduceAsync(history);
