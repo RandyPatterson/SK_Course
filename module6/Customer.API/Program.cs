a0d16b667705b83f886174909c7d4cc91fdb4a57
RandyPatterson <randy@randypatterson.com>
2025-07-31 19:43:04 -0400

    add missing module6

---
+++ module6/Customer.API/Program.cs
@@ -0,0 +1,33 @@
+using Customer.API.Services;
+using System.Reflection;
+using System.IO;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+// Register the CustomerRepository as a singleton since it's our in-memory database
+builder.Services.AddSingleton<CustomerRepository>();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer API V1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
